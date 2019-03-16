68cd08826daafb7a929027413949b7156d105295
xsobrietyx <vadym.serdiuk@gmail.com>
2019-03-16 19:21:01 +0200

    Code style.

--- src/main/scala/service/abstarctions/IOTService.scala
+++ src/main/scala/service/abstarctions/IOTService.scala
@@ -8,6 +8,7 @@ import service.abstarctions.RequestedInformation.RequestedInformation
   * Created by xsobrietyx on 12-March-2019 time 16:43
   */
 trait IOTService {
-  def getData(typeOfDevice: DeviceType,typeOfData: RequestedInformation): BigDecimal
+  def getData(typeOfDevice: DeviceType, typeOfData: RequestedInformation): BigDecimal
+
   def addDevice(device: Device): Unit
 }
